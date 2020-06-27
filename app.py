523e18512557f190acb558fa1be2a8353e99a88e
Travis CI <travis@travis-ci.org>
2020-06-27 18:13:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ import PySimpleGUI as sg
 sg.theme('DarkAmber')
 
 layout = [[sg.In(enable_events=True, do_not_clear=False, key='_IN_', size=(30, 3))],
-          [sg.Button('7', size=(5, 1), key='_BUTTON_7_'), sg.Button('8', size=(5, 1), key='_BUTTON_8_'),
-           sg.Button('9', size=(5, 1), key='_BUTTON_9_'),
+          [sg.Button('7', size=(5, 1)), sg.Button('8', size=(5, 1)),
+           sg.Button('9', size=(5, 1)),
            sg.Button(' + ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_SUM_')],
-          [sg.Button('4', size=(5, 1), key='_BUTTON_4_'), sg.Button('5', size=(5, 1), key='_BUTTON_5_'),
-           sg.Button('6', size=(5, 1), key='_BUTTON_6_'),
+          [sg.Button('4', size=(5, 1)), sg.Button('5', size=(5, 1)),
+           sg.Button('6', size=(5, 1)),
            sg.Button(' - ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_SUB_')],
-          [sg.Button('1', size=(5, 1), key='_BUTTON_1_'), sg.Button('2', size=(5, 1), key='_BUTTON_2_'),
-           sg.Button('3', size=(5, 1), key='_BUTTON_3_'),
+          [sg.Button('1', size=(5, 1)), sg.Button('2', size=(5, 1)),
+           sg.Button('3', size=(5, 1)),
            sg.Button(' x ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_M_')],
           [sg.Button(' ⌫  ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_DEL_'),
-           sg.Button('0', size=(5, 1), key='_BUTTON_0_'),
+           sg.Button('0', size=(5, 1)),
            sg.Button('=', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_RESULT_'),
            sg.Button(' ÷ ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_DIV_')]
           ]
@@ -23,28 +23,16 @@ window = sg.Window('Calculadora', layout, return_keyboard_events=True)
 while True:
     event, values = window.read()
 
-    num = float(values['_IN_']);
+    #num = float(values['_IN_']);
 
-    if event == '_BUTTON_1_':
-        print(num)
-    if event == '_BUTTON_2_':
-        print(event);
-    if event == '_BUTTON_3_':
-        print(event);
-    if event == '_BUTTON_4_':
-        print(event);
-    if event == '_BUTTON_5_':
-        print(event);
-    if event == '_BUTTON_6_':
-        print(event);
-    if event == '_BUTTON_7_':
-        print(event);
-    if event == '_BUTTON_8_':
-        print(event);
-    if event == '_BUTTON_9_':
-        print(event);
-    if event == '_BUTTON_0_':
-        print(event);
+    if event == '_BUTTON_DEL_':  # clear keys if clear button
+        keys_entered = '';
+    elif event in '1234567890':
+        keys_entered = values['_IN_']  # get what's been entered so far
+        keys_entered += event
+        print(keys_entered)
+        window['_IN_'].update(value=[keys_entered])
+
     if event == '_BUTTON_SUM_':
         n1 = num;
         op = "soma";
