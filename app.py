097653b91e81daf8c4c846df6c4fad99d401c3a4
Travis CI <travis@travis-ci.org>
2020-06-27 23:24:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
 
-layout = [[sg.In(enable_events=True, do_not_clear=False, key='_IN_', size=(30, 3))],
+layout = [[sg.In(enable_events=True, do_not_clear=True, key='_IN_', size=(30, 1))],
           [sg.Button('7', size=(5, 1)), sg.Button('8', size=(5, 1)),
            sg.Button('9', size=(5, 1)),
            sg.Button(' + ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_SUM_')],
@@ -12,57 +12,58 @@ layout = [[sg.In(enable_events=True, do_not_clear=False, key='_IN_', size=(30, 3
           [sg.Button('1', size=(5, 1)), sg.Button('2', size=(5, 1)),
            sg.Button('3', size=(5, 1)),
            sg.Button(' x ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_M_')],
-          [sg.Button(' ⌫  ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_DEL_'),
+          [sg.Button(' CE  ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_DEL_'),
            sg.Button('0', size=(5, 1)),
            sg.Button('=', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_RESULT_'),
            sg.Button(' ÷ ', button_color=('black', 'orange'), size=(5, 1), key='_BUTTON_DIV_')]
           ]
 
-window = sg.Window('Calculadora', layout, return_keyboard_events=True)
+window = sg.Window('Calculadora', layout, return_keyboard_events=False)
+keys_entered = ''
 
 while True:
     event, values = window.read()
 
-    #num = float(values['_IN_']);
-
     if event == '_BUTTON_DEL_':
         keys_entered = '';
+        n1 = 0;
+        n2 = 0;
+        window['_IN_'].update(value=[0])
+
     elif event in '1234567890':
-        keys_entered = values['_IN_']
         keys_entered += event
         print(keys_entered)
         window['_IN_'].update(value=[keys_entered])
 
-    if event == '_BUTTON_SUM_':
-        n1 = num;
+    elif event == '_BUTTON_SUM_':
+        n1 = int(keys_entered);
+        keys_entered = '';
         op = "soma";
-        window['_IN_'].update(value=[0])
-    if event == '_BUTTON_SUB_':
-        n1 = num;
+    elif event == '_BUTTON_SUB_':
+        n1 = int(keys_entered);
+        keys_entered = '';
         op = "sub";
-        window['_IN_'].update(value=[0])
-    if event == '_BUTTON_M_':
-        n1 = num;
+    elif event == '_BUTTON_M_':
+        n1 = int(keys_entered);
+        keys_entered = '';
         op = "mult";
-        window['_IN_'].update(value=[0])
-    if event == '_BUTTON_DIV_':
-        n1 = num;
+    elif event == '_BUTTON_DIV_':
+        n1 = int(keys_entered);
+        keys_entered = '';
         op = "div";
-        window['_IN_'].update(value=[0])
-    if event == '_BUTTON_DEL_':
-        print(event);
-    if event == '_BUTTON_RESULT_':
-        n2 = num;
+
+    elif event == '_BUTTON_RESULT_':
+        n2 = int(keys_entered);
         if op == 'soma':
             soma = n1+n2
             window['_IN_'].update(value=[soma]);
-        if op == 'sub':
+        elif op == 'sub':
             sub = n1-n2
             window['_IN_'].update(value=[sub]);
-        if op == 'mult':
+        elif op == 'mult':
             mult = n1*n2
             window['_IN_'].update(value=[mult]);
-        if op == 'div':
+        elif op == 'div':
             div = n1/n2
             window['_IN_'].update(value=[div]);
     if event == sg.WIN_CLOSED:
